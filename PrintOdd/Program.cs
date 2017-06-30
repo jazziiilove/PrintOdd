9ea6d2abdc843b7067fb784ebf742f468ba5c40f
baran topal <barantopal@barantopal.com>
2017-06-30 20:06:16 +0200

    initial commit

---
+++ PrintOdd/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * Programmer: Baran Topal 
+ * Solution name: PrintOdd 
+ * Project name: PrintOdd
+ * File name: Program.cs
+ * Status: Finished
+ */
+
+using System;
+
+namespace PrintOdd
+{
+    /// <summary>
+    /// prints the odd numbers between 0 and 100
+    /// </summary>
+    class Program
+    {
+        #region methods
+
+        /// <summary>
+        /// printing the odd number in recursion
+        /// </summary>
+        /// <param name="number">number</param>
+        public static void PrintOdd(int number)
+        {
+            if (number >= 1)
+            {
+                Console.Write("{0}, ", number);
+                number -= 2;
+                PrintOdd(number);
+            }
+        }
+
+        /// <summary>
+        /// Main gate
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            const int number = 99;
+
+            Console.WriteLine("Odd numbers: ");
+            PrintOdd(number);
+        }
+        #endregion methods
+    }
+}
